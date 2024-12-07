65c07c2349ab0233eae36ae4932bebd9dd807a0d
Darja Petrakova <darja.petr53@gmail.com>
2024-12-07 17:22:31 +0200

    Added 2 more game levels.

--- src/main/java/coursework/GameRules.java
+++ src/main/java/coursework/GameRules.java
@@ -16,7 +16,7 @@ public class GameRules {
     private Pane pane;
     private Canvas canvas;
     private GraphicsContext gc;
-    private Image levelImage, wonImage, loseImage;
+    private Image wonImage, loseImage, startImage;
 
     private static final int CELL_SIZE = 20;
 
@@ -27,31 +27,55 @@ public class GameRules {
     private Enemy enemy1, enemy2, enemy3;
     private GameState gameState;
 
-    public GameRules () {
+    private List<String> mapFiles = List.of("map1.txt", "map2.txt", "map3.txt");
+    private int currentLevel;
+
+    public GameRules() {
         gameState = GameState.PLAYING;
-        player = new Player(CELL_SIZE, 7 * CELL_SIZE, 1, 0, CELL_SIZE /4, CELL_SIZE,3);
-        enemy1 = new Enemy(8 * CELL_SIZE, 2 * CELL_SIZE, 0, 1, CELL_SIZE, 15);
-        enemy2 = new Enemy(15 * CELL_SIZE, 12 * CELL_SIZE, 0, -1, CELL_SIZE, 15);
-        enemy3 = new Enemy(22 * CELL_SIZE, 2 * CELL_SIZE, 0, 1, CELL_SIZE, 15);
-        map = new Map("map1.txt", CELL_SIZE);
-        canvas = new Canvas(map.getWidth() * CELL_SIZE, map.getHeight() * CELL_SIZE);
-        gc = canvas.getGraphicsContext2D();
+        currentLevel = 0;
+
+        startImage = new Image(getClass().getResource("/start.png").toExternalForm());
+        wonImage = new Image(getClass().getResource("/won.png").toExternalForm());
+        loseImage = new Image(getClass().getResource("/lose.png").toExternalForm());
+
         pane = new Pane();
+        canvas = new Canvas();
+        gc = canvas.getGraphicsContext2D();
         pane.getChildren().add(canvas);
         scene = new Scene(pane);
 
         scene.setOnKeyPressed(event -> activeKeys.add(event.getCode()));
         scene.setOnKeyReleased(event -> activeKeys.remove(event.getCode()));
 
-        levelImage = new Image(getClass().getResource("/level1.png").toExternalForm());
-        wonImage = new Image(getClass().getResource("/won.png").toExternalForm());
-        loseImage = new Image(getClass().getResource("/lose.png").toExternalForm());
-
-        startGameLoop(player, map);
+        loadLevel(currentLevel);
+        startGameLoop();
     }
 
-    private void startGameLoop (Player player, Map map) {
-        gc.drawImage(levelImage, 0, 0, canvas.getWidth(), canvas.getHeight());
+    private void loadLevel(int level) {
+        map = new Map(mapFiles.get(level), CELL_SIZE);
+        canvas.setWidth(map.getWidth() * CELL_SIZE);
+        canvas.setHeight(map.getHeight() * CELL_SIZE);
+
+        if (level == 0) {
+            player = new Player(CELL_SIZE, 7 * CELL_SIZE, 1, 0, CELL_SIZE / 4, CELL_SIZE, 3);
+            enemy1 = new Enemy(8 * CELL_SIZE, 2 * CELL_SIZE, 0, 1, CELL_SIZE, 15);
+            enemy2 = new Enemy(15 * CELL_SIZE, 12 * CELL_SIZE, 0, -1, CELL_SIZE, 15);
+            enemy3 = new Enemy(22 * CELL_SIZE, 2 * CELL_SIZE, 0, 1, CELL_SIZE, 15);
+        } else if (level == 1) {
+            player = new Player(CELL_SIZE, 2 * CELL_SIZE, 1, 0, CELL_SIZE / 4, CELL_SIZE, 3);
+            enemy1 = new Enemy(15 * CELL_SIZE, 2 * CELL_SIZE, 0, 1, CELL_SIZE, 15);
+            enemy2 = new Enemy(9 * CELL_SIZE, 12 * CELL_SIZE, 0, -1, CELL_SIZE, 15);
+            enemy3 = new Enemy(21 * CELL_SIZE, 12 * CELL_SIZE, 0, -1, CELL_SIZE, 15);
+        } else if (level == 2) {
+            player = new Player(26 * CELL_SIZE, 14 * CELL_SIZE, 1, 0, CELL_SIZE / 4, CELL_SIZE, 3);
+            enemy1 = new Enemy(8 * CELL_SIZE, 8 * CELL_SIZE, -1, 0, CELL_SIZE, 15);
+            enemy2 = new Enemy(15 * CELL_SIZE, 12 * CELL_SIZE, 0, -1, CELL_SIZE, 15);
+            enemy3 = new Enemy(22 * CELL_SIZE, 9 * CELL_SIZE, 1, 0, CELL_SIZE, 15);
+        }
+    }
+
+    private void startGameLoop() {
+        gc.drawImage(startImage, 0, 0, canvas.getWidth(), canvas.getHeight());
         PauseTransition pause = new PauseTransition(Duration.seconds(3));
         pause.setOnFinished(event -> {
             AnimationTimer gameLoop = new AnimationTimer() {
@@ -63,9 +87,14 @@ public class GameRules {
                         render();
 
                         if (player.isAtExit(map)) {
-                            gameState = GameState.LEVEL_COMPLETE;
+                            currentLevel++;
+                            if (currentLevel < mapFiles.size()) {
+                                loadLevel(currentLevel);
+                            } else {
+                                gameState = GameState.LEVEL_COMPLETE; // Game is won
+                            }
                         } else if (!player.isAlive()) {
-                            gameState = GameState.GAME_OVER;
+                            gameState = GameState.GAME_OVER; // Game over
                         }
                     } else if (gameState == GameState.LEVEL_COMPLETE) {
                         gc.drawImage(wonImage, 0, 0, canvas.getWidth(), canvas.getHeight());
@@ -81,52 +110,34 @@ public class GameRules {
 
     private void update() {
         playerLasers.forEach(laser -> laser.update(map));
-        if(enemy1.isAlive()) {
-            for (Laser laser : playerLasers) {
-                laser.checkPlayerLaserHits(laser, enemy1);
-            }
-        }
-        if(enemy2.isAlive()) {
-            for (Laser laser : playerLasers) {
-                laser.checkPlayerLaserHits(laser, enemy2);
-            }
-        }
-        if(enemy3.isAlive()) {
-            for (Laser laser : playerLasers) {
-                laser.checkPlayerLaserHits(laser, enemy3);
-            }
+
+        for (Laser laser : playerLasers) {
+            if (enemy1.isAlive()) laser.checkPlayerLaserHits(laser, enemy1);
+            if (enemy2.isAlive()) laser.checkPlayerLaserHits(laser, enemy2);
+            if (enemy3.isAlive()) laser.checkPlayerLaserHits(laser, enemy3);
         }
 
         enemy1.update(map);
         enemy2.update(map);
         enemy3.update(map);
-        if (player.isAlive()) {
-            Laser enemyLaser = enemy1.getLaser();
+
+        Laser enemyLaser;
+        if (player.isAlive() && (enemyLaser = enemy1.getLaser()) != null) {
             enemyLaser.checkEnemyLaserHits(enemyLaser, player);
         }
-        if (player.isAlive()) {
-            Laser enemyLaser = enemy2.getLaser();
+        if (player.isAlive() && (enemyLaser = enemy2.getLaser()) != null) {
             enemyLaser.checkEnemyLaserHits(enemyLaser, player);
         }
-        if (player.isAlive()) {
-            Laser enemyLaser = enemy3.getLaser();
+        if (player.isAlive() && (enemyLaser = enemy3.getLaser()) != null) {
             enemyLaser.checkEnemyLaserHits(enemyLaser, player);
         }
     }
 
     private void handleInput() {
-        if (activeKeys.contains(KeyCode.W)) {
-            player.moveUp(map, enemy1, enemy2, enemy3);
-        }
-        if (activeKeys.contains(KeyCode.S)) {
-            player.moveDown(map, enemy1, enemy2, enemy3);
-        }
-        if (activeKeys.contains(KeyCode.A)) {
-            player.moveLeft(map, enemy1, enemy2, enemy3);
-        }
-        if (activeKeys.contains(KeyCode.D)) {
-            player.moveRight(map, enemy1, enemy2, enemy3);
-        }
+        if (activeKeys.contains(KeyCode.W)) player.moveUp(map, enemy1, enemy2, enemy3);
+        if (activeKeys.contains(KeyCode.S)) player.moveDown(map, enemy1, enemy2, enemy3);
+        if (activeKeys.contains(KeyCode.A)) player.moveLeft(map, enemy1, enemy2, enemy3);
+        if (activeKeys.contains(KeyCode.D)) player.moveRight(map, enemy1, enemy2, enemy3);
         if (activeKeys.contains(KeyCode.SPACE)) {
             Laser laser = new Laser(player.getX(), player.getY(), player.getDx(), player.getDy(), CELL_SIZE);
             playerLasers.add(laser);
@@ -135,6 +146,7 @@ public class GameRules {
 
     private void render() {
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        //gc.drawImage(levelImages.get(currentLevel), 0, 0, canvas.getWidth(), canvas.getHeight());
         map.render(gc);
         player.render(gc);
         enemy1.render(gc);
